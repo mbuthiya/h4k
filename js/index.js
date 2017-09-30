ef30c138fff5a769e1ba144da1f842ce5ff4de2c
mbuthiya <jamesmuriukimbuthia@gmail.com>
2017-09-30 07:04:32 +0300

    Finished inndex page development

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $('#open').click(function () {
+
+    document.getElementById("mySidenav").style.width = "250px";
+
+  })
+
+  $('#close').click(function () {
+
+  document.getElementById("mySidenav").style.width = "0";
+
+  })
+
+})
